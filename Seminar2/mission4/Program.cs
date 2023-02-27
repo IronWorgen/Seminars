c28a29942c5f679f84bfdd5e3ff14704abe197e0
ivasenko <ivasenko98@list.ru>
2023-02-27 21:46:14 +0300

    sem 2 end

---
+++ Seminar2/mission4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.Write("введите число  ");
+int num=  int.Parse(Console.ReadLine());
+
+if (num % 7 == 0  & num % 23 == 0 )
+
+{
+    Console.Write("да");
+}
+else 
+{
+    Console.Write("нет");
+}
